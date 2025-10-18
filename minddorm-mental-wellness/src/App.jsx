79465dd7137e0adf8db5a2b1fd6b9a9a4b36f37c
Aneshya Das <aneshya512@gmail.com>
2025-10-18 23:30:13 +0530

    Update App.jsx

--- minddorm-mental-wellness/src/App.jsx
+++ minddorm-mental-wellness/src/App.jsx
@@ -25,10 +25,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Header from "./pages/Header"; // Optional: include your header
 import HomePage from "./pages/Home/HomePage";
-import ArticlesPage from "./pages/resources/articles_page";
-import VideoPage from "./pages/resources/video_page";
-import AudioPage from "./pages/resources/audio_page";
-import SupportPage from "./pages/resources/support_condition";
+import Resources from "./pages/resources/Resources";
 import WellnessDashboard from "./pages/UserDasboard/WellnessDashboard";
 import AdminDashboard from "./pages/AdminDashboard/AdminDashboard";
 
@@ -38,10 +35,7 @@ function App() {
       <Header /> {/* Optional: persistent header across pages */}
       <Routes>
         <Route path="/HomePage" element={<HomePage />} />
-        <Route path="/articles_page" element={<ArticlesPage />} />
-        <Route path="/video_page" element={<VideoPage />} />
-        <Route path="/audio_page" element={<AudioPage />} />
-        <Route path="/support_condition" element={<SupportPage />} />
+        <Route path="/resources" element={<Resources />} />
         <Route path="/WellnessDashboard" element={<WellnessDashboard />} />
         <Route path="/AdminDashboard" element={<AdminDashboard />} />
       </Routes>
